aa0c296cbcc72caeb5c052757a697de8f0e4187a
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-01 20:42:47 -0300

    Add CostCenter model

--- cost_center/models.py
+++ cost_center/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class CostCenter(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Centro de Custo'
+        verbose_name_plural = 'Centros de Custo'
+    
