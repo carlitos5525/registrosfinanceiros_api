2ed0aa96ec569ccee15da8836a242a8a03942370
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-01 20:18:49 -0300

    Add Register model

--- finance_register/models.py
+++ finance_register/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Register(models.Model):
+    STATUS_CHOICES = (
+        ('LT', 'Late'),
+        ('PD', 'Paid'),
+        ('PR', 'Predicted')
+    )
+
+    PAYMENT_METHOD_CHOICES = (
+        ('B', 'Boleto'),
+        ('CD', 'Cartão de débito'),
+        ('CC', 'Cartão de crédito'),
+        ('TED', 'TED'),
+        ('DOC', 'DOC'),
+        ('PIX', 'PIX'),
+        ('CHQ', 'Cheque'),
+        ('DDA', 'DDA')
+    )
+    
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=250)
+    due_date = models.DateField()
+    pay_date = models.DateField()
+    status = models.CharField(max_length=2,choices=STATUS_CHOICES)
+    payment_method = models.CharField(max_length=3, choices=PAYMENT_METHOD_CHOICES)
+
+    class Meta:
+        verbose_name = 'Registro'
+        verbose_name_plural = 'Registros'
+
+    
--- registros_api/settings.py
+++ registros_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'company',
+    'cost_center',
+    'finance_register',
 ]
 
 MIDDLEWARE = [
