dfeae050e21bc36d3cb590472ef9bdfaa51a7ff2
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-01 20:53:06 -0300

    Add Company and BankAcount models

--- company/models.py
+++ company/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = 'Empresa'
+        verbose_name_plural = 'Empresas'
+
+
+class BankAccount(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    bank_name = models.CharField(max_length=50)
+    bank_number = models.PositiveIntegerField()
+    agency_number = models.CharField(max_length=10)
+    account_number = models.CharField(max_length=12)
+    account_digit = models.CharField(max_length=1)
+
+    class Meta:
+        verbose_name = "Conta Bancária"
+        verbose_name_plural = "Contas Bancárias"
