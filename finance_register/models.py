2a8699c6ca765faffddad8ed6f68e06814faf18e
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-01 20:33:40 -0300

    Add Ammount model

--- finance_register/models.py
+++ finance_register/models.py
@@ -1,3 +1,4 @@
+from typing import TYPE_CHECKING
 from django.db import models
 import uuid
 
@@ -23,7 +24,7 @@ class Register(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=250)
     due_date = models.DateField()
-    pay_date = models.DateField()
+    pay_date = models.DateField(blank=True, null=True)
     status = models.CharField(max_length=2,choices=STATUS_CHOICES)
     payment_method = models.CharField(max_length=3, choices=PAYMENT_METHOD_CHOICES)
 
@@ -31,4 +32,18 @@ class Register(models.Model):
         verbose_name = 'Registro'
         verbose_name_plural = 'Registros'
 
-    
+
+class Ammount(models.Model):
+    TYPE_CHOICES = (
+        ('F', 'Fixo'),
+        ('V', 'Variavel')
+    )
+
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    type = models.CharField(max_length=2, blank=True, null=True, choices=TYPE_CHOICES)
+    finance_register = models.ForeignKey(Register, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Lançamento'
+        verbose_name_plural = 'Lançamentos'
