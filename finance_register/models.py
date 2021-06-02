be4d136110e6491257b21c388731f1a2804b602b
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-01 21:10:03 -0300

    Models first migrations

--- finance_register/models.py
+++ finance_register/models.py
@@ -48,7 +48,7 @@ class Ammount(models.Model):
     type = models.CharField(max_length=2, blank=True, null=True, choices=TYPE_CHOICES)
     finance_register = models.ForeignKey(Register, on_delete=models.CASCADE)
     company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
-    cost_center_id = models.ForeignKey(CostCenter, on_delete=models.SET_NULL)
+    cost_center_id = models.ForeignKey(CostCenter, on_delete=models.SET_NULL, null=True)
 
 
     class Meta:
