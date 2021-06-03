07b4d94f9ab8c743de2e1c18185d6b3c983ef0bb
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-03 19:58:51 -0300

    Add ammount nested relationship at Registros endpoint

--- finance_register/serializers.py
+++ finance_register/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import fields
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.serializers import Serializer, ModelSerializer
 from finance_register.models import Register
+from finance_register.models import Ammount
 
+
+class AmmountSerializer(ModelSerializer):
+    class Meta:
+        model = Ammount
+        fields = ('name', 'value', 'type', 'cost_center_id')
 
 class RegisterSerializer(ModelSerializer):
+    ammounts = AmmountSerializer(many=True)
     class Meta:
         model = Register
-        fields = ('id', 'name', 'due_date', 'pay_date', 'status', 'payment_method')
+        fields ='__all__'
