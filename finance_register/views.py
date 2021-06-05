1de3b15bcae57700ca5ff19992074faee4e8ebb6
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-05 17:36:54 -0300

    Add delete_ammount at Registro endpoint

--- finance_register/views.py
+++ finance_register/views.py
@@ -3,8 +3,9 @@ from rest_framework.viewsets import ModelViewSet
 from finance_register.models import Ammount, Register
 from finance_register.serializers import RegisterSerializer, AmmountSerializer
 from rest_framework.decorators import action
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, response
 from rest_framework.renderers import JSONRenderer
+from django.shortcuts import render, get_object_or_404
 
 
 class RegisterViewSet(ModelViewSet):
@@ -23,8 +24,8 @@ class RegisterViewSet(ModelViewSet):
         for attr, value in ammount_data.items():
             setattr(ammount, attr, value)
         ammount.save()
-        ammount_serialized = AmmountSerializer(ammount)
-        json = JSONRenderer().render(ammount_serialized.data)
+        register_serialized = RegisterSerializer(register)
+        json = JSONRenderer().render(register_serialized.data)
         return HttpResponse(json)
 
     @action(methods=['post'], detail=True)
@@ -38,3 +39,18 @@ class RegisterViewSet(ModelViewSet):
         json = JSONRenderer().render(register_serialized.data)
         return HttpResponse(json)
 
+    @action(methods=['delete'], detail=True)
+    def delete_ammount(self, request, pk):
+        ammount_id = request.data['id']
+        register = Register.objects.get(id=pk)
+        register_ammounts = register.ammounts.all()
+        ammount = get_object_or_404(Ammount, id=ammount_id)
+        if ammount in register_ammounts:
+            ammount.delete()
+            register_serialized = RegisterSerializer(register)
+            json = JSONRenderer().render(register_serialized.data)
+            return HttpResponse(json)
+        else:
+            response = HttpResponse('The ammount is not related to the finance register')
+            response.status_code = 404
+            return response
