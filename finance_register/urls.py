2bd3251fa40e5a611a29df4855f06ffeb0ade308
Carlos Mayer <carlitos5525.cap@gmail.com>
2021-06-09 18:47:22 -0300

    Add finance-register/ammounts endpoint

--- finance_register/urls.py
+++ finance_register/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path, include
 from finance_register import views
 from rest_framework import routers
-
+from rest_framework_nested import routers
 
 
 app_name='finance_register'
 
 router = routers.DefaultRouter()
 router.register(r'', views.RegisterViewSet, basename='Register')
+
+register_router = routers.NestedDefaultRouter(
+    router, r'', lookup='register'
+)
+register_router.register(
+    r'ammounts', views.AmmountViewSet, basename='Ammount'
+)
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(register_router.urls))
 ]
